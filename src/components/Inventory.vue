1ea0fcf846ef86437b40f5fac4db999c0e51acfd
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2021-12-14 18:07:12 -0600

    terminado crud de productos

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -27,22 +27,17 @@
         <div class="col-12">
           <div class="card">
             <div class="card-header">
-              <h3 class="card-title">Inventory table </h3>
-              <button class="btn btn-primary btn-sm" @click="addItem()" style="right-margin:20px" data-toggle="modal" data-target="#addItem">
+              <h3 class="card-title">Inventory table</h3>
+            </div>
+            <div class="card-header">
+              <button
+                class="btn btn-primary btn-sm bg-green"
+                v-on:click="addItem"
+                style="right-margin: 20px"
+              >
                 <i class="fas fa-plus"></i>
                 Add Item
               </button>
-            </div>
-            <div class="card-header">
-              <div>
-                <!-- <router-link
-                  :to="{ name: 'createProduct' }"
-                  class="btn btn-primary"
-                >
-                  <i class="fas fa-plus-circle"> </i>
-                  Add Item
-                </router-link> -->
-              </div>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
@@ -67,18 +62,15 @@
                       <td>{{ item.description }}</td>
                       <td class="align-middle">
                         <div class="btn-toolbar">
-                          <router-link
-                            :to="{
-                              name: 'editProduct',
-                              params: { id: item.id },
-                            }"
+                          <button
                             class="btn btn-primary btn-sm"
+                            v-on:click="editItem(item)"
                           >
-                            <i class="fas fa-edit"> </i>
-                          </router-link>
+                            <i class="fas fa-edit"></i>
+                          </button>
                           <button
                             class="btn btn-danger btn-sm"
-                            @click="deleteItem(item.id)"
+                            v-on:click="deleteItem(item.id)"
                           >
                             <i class="fas fa-trash"> </i>
                           </button>
@@ -87,52 +79,26 @@
                     </tr>
                   </tbody>
                 </table>
-                
               </div>
             </div>
             <!-- /.card-body -->
           </div>
         </div>
-
-<!-- Definicion de Modal como formulario para captura de datos -->
-      <div class="modal fade" id="addItem" aria-labelledby="addItemLabel" tabindex="-1">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="addItemLabel">Añadir Objeto</h5>
-            </div>
-            <div class="modal-body">
-          
-            </div>
-          </div>        
-        </div>
-      
-      </div>
-
       </div>
     </div>
   </section>
-
 </template>
 
 
 <script>
-
-let url = 'http://localhost:8000/api/products';
+let url = "http://localhost:8000/api/products";
+let Swal = require("sweetalert2");
 
 export default {
   name: "Inventory",
   data() {
     return {
-      items: [
-        // {
-        //   id: 1,
-        //   name: "Herramienta",
-        //   price: "8.00",
-        //   quantity: "123",
-        //   description: "Herramienta básica para la elaboración de productos",
-        // },
-      ],
+      items: [],
     };
   },
   created() {
@@ -145,34 +111,110 @@ export default {
       });
     },
     addItem() {
-      this.axios
-        .post(url, {
-          name: this.name,
-          price: this.price,
-          stock: this.stock,
-          description: this.description,
-        })
-        .then((response) => {
-          this.items.push(response.data);
-          this.name = "";
-          this.price = "";
-          this.stock = "";
-          this.description = "";
-        });
-        /*
-        addItem2: async function()
-    {
-      const{value: formvalues} = await this.$swal.fire({
-        title:'Añadir',
-        html:'',
-        focusConfirm: false,
+      Swal.fire({
+        title: "Añadir Objeto",
+        html:
+          '<input id="swal-input1" class="swal2-input" placeholder="Nombre">' +
+          '<input id="swal-input2" class="swal2-input" type="number" placeholder="Precio">' +
+          '<input id="swal-input3" class="swal2-input" type="number" placeholder="Cantidad">' +
+          '<input id="swal-input4" class="swal2-input" placeholder="Descripción">',
         showCancelButton: true,
-        confirmButtonText: 'Gurdar',
-        cancelButtonColor:'#3085d6',
-        preConfirm: () =>{
-          return[]
+        focusConfirm: false,
+        preConfirm: () => {
+          return [
+            document.getElementById("swal-input1").value,
+            document.getElementById("swal-input2").value,
+            document.getElementById("swal-input3").value,
+            document.getElementById("swal-input4").value,
+          ];
+        },
+      }).then((result) => {
+        if (result.value) {
+          this.axios
+            .post(url, {
+              name: result.value[0],
+              price: result.value[1],
+              stock: result.value[2],
+              description: result.value[3],
+            })
+            .then(() => {
+              this.getItems();
+              this.name = "";
+              this.price = "";
+              this.stock = "";
+              this.description = "";
+            });
+          Swal.fire("Añadido!", "El objeto ha sido añadido.", "success");
+        } else {
+          Swal.fire("Cancelado", "El objeto no ha sido añadido.", "error");
         }
-      })*/
+      });
+    },
+    deleteItem(id) {
+      Swal.fire({
+        title: "¿Estás seguro?",
+        text: "No podrás revertir esto!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, borrarlo!",
+      }).then((result) => {
+        if (result.value) {
+          this.axios.delete(url + "/" + id).then(() => {
+            this.getItems();
+            Swal.fire("Borrado!", "El objeto ha sido borrado.", "success");
+          });
+        }
+      });
+    },
+    editItem(item) {
+      Swal.fire({
+        title: "Editar Objeto",
+        html:
+          '<input id="swal-input1" class="swal2-input" value="' +
+          item.name +
+          '">' +
+          '<input id="swal-input2" class="swal2-input" type="number" value="' +
+          item.price +
+          '">' +
+          '<input id="swal-input3" class="swal2-input" type="number" value="' +
+          item.stock +
+          '">' +
+          '<input id="swal-input4" class="swal2-input" value="' +
+          item.description +
+          '">',
+        showCancelButton: true,
+        focusConfirm: false,
+        preConfirm: () => {
+          return [
+            document.getElementById("swal-input1").value,
+            document.getElementById("swal-input2").value,
+            document.getElementById("swal-input3").value,
+            document.getElementById("swal-input4").value,
+          ];
+        },
+      }).then((result) => {
+        if (result.value) {
+          this.axios
+            .put(url + "/" + item.id, {
+              name: result.value[0],
+              price: result.value[1],
+              stock: result.value[2],
+              description: result.value[3],
+            })
+            .then(() => {
+              this.getItems();
+              this.name = "";
+              this.price = "";
+              this.stock = "";
+              this.description = "";
+            });
+          Swal.fire("Editado!", "El objeto ha sido editado.", "success");
+        } else {
+          Swal.fire("Cancelado", "El objeto no ha sido editado.", "error");
+        }
+      });
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -4,15 +4,17 @@ import VueSidebarMenu from 'vue-sidebar-menu'
 import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 import router from './router'
 import axios from 'axios'
-import  VueAxios from 'vue-axios'
+import VueAxios from 'vue-axios'
 import bootstrap from 'bootstrap'
 //import Swal from 'sweetalert2/dist/sweetalert2.js'
+// import sweetalert2 from 'sweetalert2'
 
 
 const app = createApp(App)
 app.use(router)
 app.use(VueSidebarMenu)
 app.use(bootstrap)
+// app.use(sweetalert2)
 //app.use(Swal)
 //app.use(axios)
 app.use(VueAxios, axios)
